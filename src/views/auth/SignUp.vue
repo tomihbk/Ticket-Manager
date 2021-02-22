1296ddace7776ede4eddf9c9c7a9de3ce31bbd9b
Thomas Berhane <thomasnegassi@gmail.com>
2021-02-22 01:38:41 +0100

    Signup page form validation improvement + FB create tech implemented

--- src/views/auth/SignUp.vue
+++ src/views/auth/SignUp.vue
@@ -13,13 +13,16 @@
         </span>
 
         <h1>Sign Up</h1>
-        <v-form ref="form" class="ma-5 signup-form" @submit.prevent="signup">
+        <v-form ref="form" class="mt-5 mx-5 signup-form" @submit.prevent="signup">
           <v-text-field v-model="name" label="Nom" :rules="[rules.required]" filled></v-text-field>
           <v-text-field v-model="sirname" label="Prénom" :rules="[rules.required]" filled></v-text-field>
-          <v-text-field v-model="email" label="Email" :rules="[rules.required, rules.email]" filled></v-text-field>
-          <v-text-field type="password" v-model="password" :rules="[rules.required]" label="Mot de Passe" filled></v-text-field>
-          <v-text-field type="password" v-model="confirmpassword" :rules="[rules.required]" label="Confirmation du mot de passe" filled></v-text-field>
-          <v-btn large dark color="green" depressed @click="validate">créer un compte</v-btn>
+          <v-text-field type="email" v-model="email" label="Email" :rules="[rules.required, rules.email]" filled></v-text-field>
+          <v-text-field type="password" v-model="password" :rules="[rules.required,rules.strongPassword]" label="Mot de Passe" filled></v-text-field>
+          <v-text-field type="password" v-model="confirmpassword" :rules="[rules.required, rules.identic]" label="Confirmation du mot de passe" filled></v-text-field>
+          <v-btn large color="green" dark depressed @click="validate"><span class="font-weight-bold">créer un compte</span> </v-btn>
+          <p class="feedback">
+            {{this.feedback}}
+          </p>
         </v-form>
         <hr style="margin:1.5em;color:red;border:.2px solid #d5d5d5;" />
         <span>Vous avez déjà un compte</span>
@@ -31,31 +34,58 @@
 </template>
 
 <script>
+import db from '@/firebase/api'
+import firebase from 'firebase'
+
 export default {
   name: 'signup',
   data () {
     return {
-      name: null,
-      sirname: null,
-      email: null,
-      password: null,
-      confirmpassword: null,
+      name: 'null',
+      sirname: 'null',
+      email: 'thomasnegassi@gmail.com',
+      password: 'Thomas619',
+      confirmpassword: 'Thomas619',
+      feedback: null,
       rules: {
         required: value => !!value || 'Champ obligatoire',
         email: value => {
           const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
           return pattern.test(value) || 'E-mail Invalide'
+        },
+        strongPassword: value => {
+          const pattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[a-zA-Z\d]{6,}$/
+          return pattern.test(value) || 'Minimum 6 caractères, au moins une lettre majuscule, une lettre minuscule et un chiffre'
+        },
+        identic: () => {
+          return this.password === this.confirmpassword || 'Les mots de passes ne sont pas identiques'
         }
       }
     }
   },
   methods: {
     validate () {
-      console.log('Validating')
       this.$refs.form.validate()
-    },
-    signup () {
 
+      if (!this.rules.required(this.name) &&
+      !this.rules.required(this.sirname) &&
+      !this.rules.email(this.email) &&
+      !this.rules.strongPassword(this.password) &&
+      !this.rules.identic(this.confirmpassword)) {
+        return
+      }
+
+      this.signup()
+    },
+    async signup () {
+      try {
+        await firebase.auth().createUserWithEmailAndPassword(this.email, this.password)
+      } catch (err) {
+        console.log(err)
+        if (err.code === 'auth/email-already-in-use') {
+          this.feedback = "L'email est déjà enregistré dans le système"
+        }
+      }
     }
   }
 }
@@ -84,10 +114,17 @@ hr {
 h1:nth-child(2) {
   margin-bottom: 1em;
 }
+
 .signup-container {
   /*background-image: url("https://walker-web.imgix.net/cms/Gradient_builder_2.jpg");*/
   background-image: url("https://products.ls.graphics/mesh-gradients/images/08.-Violet-Blue.jpg");
   background-position: center left;
   height: 100vh;
 }
+
+.signup-container  .feedback{
+    color: red;
+margin-top: 2em;
+margin-bottom: 0;
+  }
 </style>
