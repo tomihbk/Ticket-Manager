2ed0e7d76da29597246b82710d5a0c2335e92d4b
Thomas Berhane <thomasnegassi@gmail.com>
2021-03-01 14:12:27 +0100

    firebase auth guard to only render app

--- src/main.js
+++ src/main.js
@@ -2,11 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
 
-new Vue({
-  router,
-  vuetify,
-  render: h => h(App)
-}).$mount('#app')
+// Wait for firebase auth before vue render
+
+let app = null
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      vuetify,
+      render: h => h(App)
+    }).$mount('#app')
+  }
+})
