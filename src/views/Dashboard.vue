535721dec66b2bb4baf065fa1f34e7685263308c
Thomas Berhane <thomasnegassi@gmail.com>
2021-03-04 01:37:10 +0100

    dashboard dummy data and button for creations

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,14 +1,59 @@
 <template>
   <div class="dashboard">
-    <h1>Dashboard</h1>
+    <span class="text-h2 font-weight-bold ma-5">Dashboard</span>
 
-    <v-container class="my-5">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni error exercitationem, iure molestiae minima iste voluptas quam sunt quidem nihil inventore corrupti fugit eveniet illo voluptatem modi vel voluptatum voluptatibus?
+    <v-container class="my-5 main-container">
+      <v-card class="mx-auto ticket-open" max-width="200" min-width="200" v-for="(btn,index) in btnData" :key="index" :class="btn.color" :to="btn.route">
+        <v-card-text class="mx-auto md-5">
+          <div class="font-weight-bold">Total</div>
+          <p class="text-h3 mt-3">
+            {{btn.total}}<v-icon large class="ml-4" color="white">{{btn.icon}}</v-icon>
+          </p>
+          <p class="font-weight-medium"> {{btn.text}}</p>
+        </v-card-text>
+      </v-card>
+
     </v-container>
+
+    <v-btn x-large depressed class="green lighten-5 ma-5" max-width="200" to="add-ticket">
+      <v-icon color="black" left>mdi-file-plus</v-icon> <span class="font-weight-bold">Créer ticket</span>
+    </v-btn>
+
+    <v-btn x-large depressed class="purple lighten-5 ma-5" max-width="200" to="add-user">
+      <v-icon color="black" left>mdi-account-plus</v-icon> <span class="font-weight-bold">Ajouter client</span>
+    </v-btn>
   </div>
 </template>
 
 <script>
 export default {
+  data () {
+    return {
+      btnData: [
+        { icon: 'mdi-folder-open-outline', text: 'Tickets ouverts', color: 'green', total: 14, route: '/tickets' },
+        { icon: 'mdi-package-variant-closed', text: 'Tickets fermés', color: 'brown', total: 35, route: '/archive' },
+        { icon: 'mdi-account-group', text: 'Clients', color: 'purple', total: 18, route: '/users' }
+      ]
+    }
+  }
 }
 </script>
+
+<style>
+.ticket-open {
+  margin-top: 2em;
+}
+.ticket-open div,
+.ticket-open p,
+.ticket-open v-icon {
+  color: white;
+}
+.dashboard {
+  text-align: center;
+  margin-top: 3em;
+}
+.dashboard .main-container {
+  display: flex;
+  flex-wrap: wrap;
+}
+</style>
