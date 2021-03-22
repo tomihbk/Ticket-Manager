e942e36a425528d480819fc84f553e501eaabd64
Thomas Berhane <thomasnegassi@gmail.com>
2021-03-22 03:20:40 +0100

    Client's profile page implemented

--- src/views/manage/ManageUser.vue
+++ src/views/manage/ManageUser.vue
@@ -1,131 +1,125 @@
 <template>
-  <div>
-    <div class="adduser-container">
-      <v-container fluid fill-height>
-        <v-sheet color="white" elevation="1" class="container ticket-sheet rounded-lg" align="center">
-
-          <h1 class="mb-5">Mettre à jour {{clientData.name}} {{clientData.surname}}</h1>
-          <v-form ref="form" class="mt-5 mx-5 signup-form" @submit.prevent="validate">
-            <v-row>
-              <v-col cols="12" md="12">
-                <v-text-field type="text" v-model="clientData.company" label="Entreprise" dense></v-text-field>
-              </v-col>
-
-              <v-col cols="12" md="6">
-                <v-text-field type="text" v-model="clientData.name" label="Nom" :rules="[rules.required]" dense></v-text-field>
-              </v-col>
-              <v-col cols="12" md="6">
-                <v-text-field type="text" v-model="clientData.surname" label="Prénom" :rules="[rules.required]" dense></v-text-field>
-              </v-col>
-              <v-col cols="12" md="12">
-                <v-text-field type="text" v-model="clientData.address.address1" label="Adresse 1" dense></v-text-field>
-              </v-col>
-
-              <v-col cols="12" md="12">
-                <v-text-field type="text" v-model="clientData.address.address2" label="Adresse 2" dense></v-text-field>
-              </v-col>
-
-              <v-col cols="12" md="12">
-                <v-text-field type="tel" v-model="clientData.mobile" label="Téléphone mobile" dense></v-text-field>
-              </v-col>
-
-              <v-col cols="12" md="12">
-                <v-text-field type="tel" v-model="clientData.fixe" label="Téléphone fixe" dense></v-text-field>
-              </v-col>
-
-              <v-col cols="12" md="4">
-                <v-text-field type="tel" v-model="clientData.address.npa" label="NPA" dense counter maxlength="4"></v-text-field>
-              </v-col>
-              <v-col cols="12" md="8">
-                <v-text-field type="text" v-model="clientData.address.locality" label="Localité" dense></v-text-field>
-              </v-col>
-              <v-col cols="12" md="12">
-                <v-text-field type="email" v-model="clientData.email" label="email" dense></v-text-field>
-              </v-col>
-            </v-row>
-            <v-btn large color="blue" :loading="loading" dark depressed @click="updateData" class="mt-5"><span class="font-weight-bold">Mettre à jour</span> </v-btn>
-            <p class="feedback">
-              {{this.feedback}}
+  <div class="manage-ticket">
+    <v-row v-if="client">
+      <v-col cols="12" md="5">
+        <v-card class="pa-5 ma-5">
+          <v-card-text class="pb-0">
+            <p class="display-1 text--primary font-weight-bold">
+              <span v-if="client.company">{{client.company}}<br /></span>
+              {{client.name}} {{client.surname}}
+              <v-btn depressed fab absolute small right direction="bottom" @click="modifyClient">
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn>
             </p>
-          </v-form>
-        </v-sheet>
-      </v-container>
-    </div>
+            <p>Information :</p>
+            <v-simple-table class="mb-5">
+              <tbody>
+                <tr v-if="client.address">
+                  <td>Adresse :</td>
+                  <td class="py-3">
+                    <span v-if="client.address.address1">{{client.address.address1}}<br></span>
+                    <span v-if="client.address.address2">{{client.address.address2}}<br></span>
+                    <span v-if="client.address.npa">{{client.address.npa}}</span> <span v-if="client.address.locality">{{client.address.locality}}</span>
+                  </td>
+                </tr>
+                <tr v-if="client.mobile">
+                  <td>Mobile :</td>
+                  <td class="py-3"><a :href="`tel:${client.mobile}`">{{client.mobile}}</a></td>
+                </tr>
+                <tr v-if="client.fixe">
+                  <td>Tel. fixe :</td>
+                  <td class="py-3"><a :href="`tel:${client.fixe}`">{{client.fixe}}</a></td>
+                </tr>
+                <tr v-if="client.email">
+                  <td>Email :</td>
+                  <td class="py-3"><a :href="`mailto:${client.email}`">{{client.email}}</a></td>
+                </tr>
+                <tr v-if="client.created">
+                  <td>Crée :</td>
+                  <td class="py-3" style="white-space: pre-line;">{{getDataTimeUnix(client.created.at.seconds)}}</td>
+                </tr>
+              </tbody>
+            </v-simple-table>
+          </v-card-text>
+        </v-card>
+      </v-col>
+      <v-col cols="12" md="7">
+        <v-card class="pa-5 ma-5">
+          <v-card-text class="pb-0">
+            <p class="display-1 text--primary font-weight-bold">
+              Historique des travaux
+            </p>
+
+            <div v-if="ticketList != []">
+              <template v-for="(ticket,i) in ticketList">
+                <v-card :key="i" @click="editTicket(ticket.id)" class="mt-5 blue lighten-5" elevation="1" rounded>
+                  <v-card-title>
+                    Ticket N° {{ticket.ticketID}}
+                  </v-card-title>
+                  <v-card-text class="pb-5 black--text">
+                    Crée : {{ticket.created.at.seconds}}<br />
+                    Titre : {{ticket.title}}<br />
+                    État : <v-chip>{{ticket.state}}</v-chip>
+                  </v-card-text>
+                </v-card>
+              </template>
+            </div>
+            <div v-else class="black--text">Pas de travaux...</div>
+          </v-card-text>
+        </v-card>
+
+      </v-col>
+    </v-row>
   </div>
 </template>
 
 <script>
+
+import moment from 'moment'
 import db from '@/firebase/api'
-import algoindex from '@/algolia/clientsIndices'
 export default {
   data: () => ({
-    firestoreID: null,
-    clientData: {
-      company: null,
-      name: null,
-      surname: null,
-      address: {
-        address1: null,
-        address2: null,
-        npa: null,
-        locality: null
-      },
-      mobile: null,
-      fixe: null,
-
-      email: null
-    },
-    loading: false,
-    feedback: null,
-    rules: {
-      required: value => !!value || 'Champ obligatoire'
-    }
-
+    client: null,
+    ticketList: []
   }),
   created () {
-    this.fillData()
+    this.initialize()
   },
   methods: {
-    fillData () {
-      const passedData = this.$route.params.client_data
-      console.log(passedData)
-      this.firestoreID = passedData.id
-      this.clientData.company = passedData.company
-      this.clientData.name = passedData.name
-      this.clientData.surname = passedData.surname
-      this.clientData.address.address1 = passedData.address.address1
-      this.clientData.address.address2 = passedData.address.address2
-      this.clientData.address.npa = passedData.address.npa
-      this.clientData.address.locality = passedData.address.locality
-      this.clientData.mobile = passedData.mobile
-      this.clientData.fixe = passedData.fixe
-      this.clientData.email = passedData.email
-    },
-    async updateData () {
-      this.$refs.form.validate()
-      if (!this.clientData.name || !this.clientData.surname) {
-        this.feedback = 'Les champs nom et prénom doivent être remplis'
-      } else {
-        this.feedback = null
+    async initialize () {
+      console.log('This is gonna cost ya')
 
-        // This removed null data recursivly from javascript objects
-        const dataWithoutNull = Object.fromEntries(
-          Object.entries(this.clientData)
-            .filter(([_, v]) => v != null)
-            .map(([k, v]) => [k, v === Object(v) ? Object.fromEntries(Object.entries(v).filter(([_, v]) => v != null)) : v])
-        )// Data has been cleaned
-        try {
-          this.loading = true
-          await db.collection('clients').doc(this.firestoreID).set(dataWithoutNull)
-          dataWithoutNull.objectID = this.firestoreID
-          await algoindex.saveObjects([dataWithoutNull])
-          this.loading = false
-          this.$router.push({ name: 'users' })
-        } catch (err) {
-          this.loading = false
-          console.log(err)
-        }
-      }
+      await db.collection('clients').doc(this.$route.params.client_id).get()
+        .then(document => {
+          const client = document.data()
+          client.id = document.id
+          this.client = client
+        })
+
+      await db.collection('tickets').where('user.id', '==', this.$route.params.client_id).get()
+        .then(snapshot => {
+          snapshot.forEach(document => {
+            console.log(document.data())
+            const tickets = document.data()
+            tickets.id = document.id
+            tickets.created.at.seconds = moment.unix(document.data().created.at.seconds).format('DD-MM-YYYY à HH:mm')
+            this.ticketList.push(tickets)
+          })
+        })
+      console.log(this.ticketList)
+    },
+    modifyClient () {
+      this.$router.push({ name: 'edituser', params: { client_id: this.$route.params.client_id } })
+    },
+    editTicket (ticketID) {
+      // get item id and send via router prop to manage page
+      this.$router.push({ name: 'manageticket', params: { ticket_id: ticketID } })
+    },
+    getDataTimeUnix (time) {
+      return moment.unix(time).format('DD-MM-YY à HH:mm:ss')
+    },
+    getDataTimeMS (time) {
+      return moment(time).format('DD-MM-YY à HH:mm:ss')
     }
   }
 }
