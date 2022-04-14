4c0d093edfb10c9958f2934d6939cec519fb00c2
Thomas <thomasnegassi@gmail.com>
2022-04-14 22:48:09 +0200

    firebase env storage bucket name fix

--- src/firebase/api.js
+++ src/firebase/api.js
@@ -4,7 +4,7 @@ var firebaseConfig = {
   apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
   authDomain: process.env.VUE_APP_FIREBASE_AUTHDOMAIN,
   projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.VUE_APP_STORAGE_BUCKET,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGIN_SENDER_ID,
   appId: process.env.VUE_APP_FIREBASE_API_ID
 }
