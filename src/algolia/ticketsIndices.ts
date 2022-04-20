14fd17612a8c67d6c7ed932f32eb427ca0959b3d
Thomas <thomasnegassi@gmail.com>
2022-04-20 15:47:42 +0200

    all js files converted into ts

--- src/algolia/ticketsIndices.ts
+++ src/algolia/ticketsIndices.ts
@@ -1,8 +1,8 @@
-import * as algoliasearch from 'algoliasearch'
+import algoliasearch from 'algoliasearch'
 
 const algolia = algoliasearch(process.env.VUE_APP_ALGOLIA_APP_ID,
   process.env.VUE_APP_ALGOLIA_API_KEY)
 
-const algoindex = algolia.initIndex(process.env.VUE_APP_ALGOLIA_INDEX_CLIENTS)
+const algoindex = algolia.initIndex(process.env.VUE_APP_ALGOLIA_INDEX_TICKETS)
 
 export default algoindex
